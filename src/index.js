5924e64cc7abcffd7e92eecb60a8e5c5314c4057
zecampos <joseguilhermesantoscampos@gmail.com>
2018-11-10 13:05:43 -0200

    cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const mysql      = require('mysql');
 const bodyParser = require('body-parser');
 const express = require('express');
+const cors = require('cors')
+
 
 const app = express();
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
